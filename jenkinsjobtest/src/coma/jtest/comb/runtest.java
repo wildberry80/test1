f8977b15c97ebddef04a246eae9bd7fac956dc1a
sidhubram <sidhubram@gmail.com>
2018-05-08 13:32:34 -0400

    second commit

--- jenkinsjobtest/src/coma/jtest/comb/runtest.java
+++ jenkinsjobtest/src/coma/jtest/comb/runtest.java
@@ -1,10 +1,19 @@
 package coma.jtest.comb;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class runtest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello jenkins");
+		System.setProperty("webdriver.chrome.driver", "C:\\Tools\\workspacejenkinstest\\chromeBrowser\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://opensource.dem.orangehrmlive.com/");
+		
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
 	}
 
 }
